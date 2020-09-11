eaf7ac74405b1b927ca19ab0783ca4349e3bbb1a
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-10 22:46:02 -0700

    created the calls in burger.js.. i think

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
 const orm = require("../config/orm");
+
+const burger = {
+    selectAll: () => {
+      orm.selectAll("burgers", (res) => {
+        console.log(res);
+      });
+    },
+    insertOne: (name, devoured) => {
+        orm.insertOne(name, devoured, (res => {
+            console.log(res);
+        }));
+    },
+
+    updateOne: (devoured, nameToUpdate) => {
+        orm.updateOne(devoured, nameToUpdate,(res => {
+            console.log(res);
+        }));
+    },
+
+  };
+
+burger.updateOne(false, "Big Bang Burger");
