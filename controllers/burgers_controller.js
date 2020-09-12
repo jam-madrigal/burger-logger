5b84a2e9b494e6093c6fe9ae8d67ae5b9b055931
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-12 11:47:59 -0700

    created the burgers_controller.js in a controllers folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne([
+      "name", "devoured"
+    ], [
+      req.body.name, req.body.devoured
+    ], (result) => {
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, (result) => {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
