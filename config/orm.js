2b9bd63ad58fa3b3a35a649b4be54783214cb032
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-13 13:22:47 -0700

    set up server.js, added views foldre with handlebars files began in them

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,8 @@
 const connection = require('./connection');
 
 const orm = {
-    selectAll: function() {
-      let queryString = "SELECT * FROM burgers";
+    selectAll: function(table) {
+      let queryString = "SELECT * FROM " + table + ";";
       connection.query(queryString, (err, result) => {
         if (err) throw err;
         console.log(result);
@@ -10,8 +10,9 @@ const orm = {
     },
 
     insertOne: function(name, devoured) {
-        let queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)"
+        let queryString = `INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)`
         console.log(queryString);
+        name.toString();
         connection.query(queryString, [name, devoured], (err, result) => {
             if (err) throw err;
             console.log(result);
@@ -21,6 +22,7 @@ const orm = {
     updateOne: function(devoured, nameToUpdate) {
         let queryString = "UPDATE burgers SET devoured = ? WHERE burger_name = ?"
         console.log(queryString);
+        nameToUpdate.toString();
         connection.query(queryString, [devoured, nameToUpdate], (err, result) => {
             if (err) throw err;
             console.log(result);
--- models/burger.js
+++ models/burger.js
@@ -20,3 +20,4 @@ const burger = {
 
   };
 
+module.exports = burger;
