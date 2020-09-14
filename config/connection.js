b5737d53a644e28cc99486da439ed9bd0f67d854
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-13 22:29:34 -0700

    changed how connectin is defined in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,24 @@
 // Dependencies
 const util = require('util');
 const mysql = require('mysql');
+let connection;
 
 // Defining the connection to make to mySQL
 if (process.env.JAWSDB_URL) {
-    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
  } else {
-const connection = mysql.createConnection({
+     connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'programmingsocks',
     database: 'burgers_db'
 });
+};
 
 // Connecting to it...
 connection.connect();
 
-};
+// This prevents the unhandled promise error from running viewAllTables() without it and allows async/await syntax 
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
