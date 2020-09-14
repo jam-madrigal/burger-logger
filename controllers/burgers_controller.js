23bfd8d5a669c82ad1d06d4213571d65d5d09ab2
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-13 20:48:51 -0700

    the delete button now functions correctly

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -43,7 +43,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
     burger.delete(
       condition, (result) => {
-      if (result.changedRows == 0) {
+      if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
